c6078c2472ea2f00041782262760e950cacb6812
Raju-kushwaha1230 <mightyrajukushwaha@gmail.com>
2025-01-20 11:30:12 +0530

    'md'

--- node-filesystem/script.js
+++ node-filesystem/script.js
@@ -1,5 +1,33 @@
+fs.appendFile('hello.txt', 'hey this side raju', (err) => {
+    if (err) {
+        console.error('Error appending to file:', err);
+    } else {
+        console.log('Data appended to file successfully');
+    }
+});
 
+fs.open('hello.txt', 'w', (err, fd) => {
+    if (err) {
+        console.error('Error opening file:', err);
+    } else {
+        console.log('File opened successfully');
+        fs.close(fd, (err) => {
+            if (err) {
+                console.error('Error closing file:', err);
+            } else {
+                console.log('File closed successfully');
+            }
+        });
+    }
+});
 
+fs.unlink('hello.txt', (err) => {
+    if (err) {
+        console.error('Error deleting file:', err);
+    } else {
+        console.log('File deleted successfully');
+    }
+});
 var fs = require('fs')
 
 // fs.appendFile('hello.txt','hey this side raju',function(err){
