777b1aef5e07101d3194bfeddedafb7958ec9c66
Raju-kushwaha1230 <mightyrajukushwaha@gmail.com>
2025-01-20 11:27:30 +0530

    'mdd'

---
+++ node filesystem/script.js
@@ -0,0 +1,18 @@
+
+
+var fs = require('fs')
+
+// fs.appendFile('hello.txt','hey this side raju',function(err){
+//     if(err) throw err;
+//     console.log('saved')
+    
+// })
+
+// fs.open('hello.txt','w',function(err,data){
+//     if(err) return err;
+//     console.log('saved')
+// })
+// fs.unlink('hello.txt',function(err){
+//     if(err) return err;
+//     console.log('file deleted')
+// })
